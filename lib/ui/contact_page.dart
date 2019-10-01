4d30e6e90793afc7ed73e22f2003c3abc519f657
criscoelhor <coelhocr@gmail.com>
2019-09-30 23:51:19 -0300

    contact_page details

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -13,6 +13,12 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+
+  bool _userEdited = false;
   Contact _editedContact;
 
   @override
@@ -23,6 +29,10 @@ class _ContactPageState extends State<ContactPage> {
       _editedContact = Contact();
     } else {
       _editedContact = Contact.fromMap(widget.contact.toMap());
+
+      _nameController.text = _editedContact.name;
+      _emailController.text = _editedContact.email;
+      _phoneController.text = _editedContact.phone;
     }
   }
 
@@ -42,20 +52,48 @@ class _ContactPageState extends State<ContactPage> {
       body: SingleChildScrollView(
         padding: EdgeInsets.all(10.0),
         child: Column(
-           children: <Widget>[
-             GestureDetector(
-               child:  Container(
-                 width: 140.0,
-                 height: 140.0,
-                 decoration: BoxDecoration(
-                     shape: BoxShape.circle,
-                     image: DecorationImage(
-                         image: _editedContact.img != null
-                             ? FileImage(File(_editedContact.img))
-                             : AssetImage("images/person.png"))),
-               ),
-             )
-           ],
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                width: 140.0,
+                height: 140.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: _editedContact.img != null
+                            ? FileImage(File(_editedContact.img))
+                            : AssetImage("images/person.png"))),
+              ),
+            ),
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: "Nome"),
+              onChanged: (text) {
+                _userEdited = true;
+                setState(() {
+                  _editedContact.name = text;
+                });
+              },
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: "E-mail"),
+              onChanged: (text) {
+                _userEdited = true;
+                _editedContact.email = text;
+              },
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: _phoneController,
+              decoration: InputDecoration(labelText: "Fone"),
+              onChanged: (text) {
+                _userEdited = true;
+                _editedContact.phone = text;
+              },
+              keyboardType: TextInputType.phone,
+            ),
+          ],
         ),
       ),
     );
