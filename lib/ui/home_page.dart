34fdf3f2e1faf96bfd5193b4fc1e4cebe8d28b9e
Cristiano Coelho Ribeiro <cristiano.ribeiro@mynarski.com>
2019-10-02 14:57:04 -0300

    add final details

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'contact_page.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-enum OrderOptions {orderaz, orderza};
+enum OrderOptions { orderaz, orderza }
 
 class HomePage extends StatefulWidget {
   @override
@@ -30,7 +30,17 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         actions: <Widget>[
           PopupMenuButton<OrderOptions>(
-            itemBuilder: ,
+            itemBuilder: (context) => <PopupMenuEntry<OrderOptions>>[
+              const PopupMenuItem<OrderOptions>(
+                child: Text("Ordenar de A_Z"),
+                value: OrderOptions.orderaz,
+              ),
+              const PopupMenuItem<OrderOptions>(
+                child: Text("Ordenar de Z_A"),
+                value: OrderOptions.orderza,
+              ),
+            ],
+            onSelected: _orderList,
           )
         ],
       ),
@@ -67,7 +77,8 @@ class _HomePageState extends State<HomePage> {
                     image: DecorationImage(
                         image: contacts[index].img != null
                             ? FileImage(File(contacts[index].img))
-                            : AssetImage("images/person.png"))),
+                            : AssetImage("images/person.png"),
+                        fit: BoxFit.cover)),
               ),
               Padding(
                 padding: EdgeInsets.only(left: 10.0),
@@ -103,61 +114,62 @@ class _HomePageState extends State<HomePage> {
   void _showOptions(BuildContext context, int index) {
     showModalBottomSheet(
         context: context,
-        builder: (context){
+        builder: (context) {
           return BottomSheet(
-            onClosing: (){},
-            builder: (context){
+            onClosing: () {},
+            builder: (context) {
               return Container(
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                   Padding(
-                     padding: EdgeInsets.all(10.0),
-                     child:  FlatButton(
-                       child: Text("Ligar",
-                         style: TextStyle(color: Colors.red, fontSize: 20.0),
-                       ),
-                       onPressed: (){
-                          launch("tel:${contacts[index].phone}");
-                          Navigator.pop(context);
-                       },
-                     ),
-                   ),
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child:  FlatButton(
-                        child: Text("Editar",
-                          style: TextStyle(color: Colors.red, fontSize: 20.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: FlatButton(
+                          child: Text(
+                            "Ligar",
+                            style: TextStyle(color: Colors.red, fontSize: 20.0),
+                          ),
+                          onPressed: () {
+                            launch("tel:${contacts[index].phone}");
+                            Navigator.pop(context);
+                          },
                         ),
-                        onPressed: (){
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: FlatButton(
+                          child: Text(
+                            "Editar",
+                            style: TextStyle(color: Colors.red, fontSize: 20.0),
+                          ),
+                          onPressed: () {
                             Navigator.pop(context);
                             _showContactPage(contact: contacts[index]);
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child:  FlatButton(
-                        child: Text("Excluir",
-                          style: TextStyle(color: Colors.red, fontSize: 20.0),
+                          },
                         ),
-                        onPressed: (){
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: FlatButton(
+                          child: Text(
+                            "Excluir",
+                            style: TextStyle(color: Colors.red, fontSize: 20.0),
+                          ),
+                          onPressed: () {
                             helper.deleteContact(contacts[index].id);
                             setState(() {
                               contacts.removeAt(index);
                               Navigator.pop(context);
                             });
-                        },
+                          },
+                        ),
                       ),
-                    ),
-                  ],
-                )
-              );
+                    ],
+                  ));
             },
           );
-        }
-    );
+        });
   }
 
   void _showContactPage({Contact contact}) async {
@@ -181,5 +193,22 @@ class _HomePageState extends State<HomePage> {
         contacts = list;
       });
     });
+  }
+
+  void _orderList(OrderOptions result) {
+    switch (result) {
+      case OrderOptions.orderaz:
+        contacts.sort((a, b) {
+          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
+        break;
+      case OrderOptions.orderza:
+        contacts.sort((a, b) {
+          return b.name.toLowerCase().compareTo(a.name.toLowerCase());
+        });
+        break;
+    }
+
+    setState(() {});
   }
 }
